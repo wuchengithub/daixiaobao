d0e3b2a24d2442e0646ba506f1e699b4fae56d99
wuchengithub <wuchenxy2015@gmail.com>
2015-03-20 17:00:51 +0800

    add
    add

---
+++ actionbarsherlock/demo.h
@@ -0,0 +1,39 @@
+
+
+
+int main(int argc ,char[] avgs) {
+	@autoreleasepool {
+		//结构体的定义和结构体变量的初始化
+		struct point {
+			int i;
+			int j;
+
+		} p1 {1,2};
+		
+		
+		//使用typeof关键字重新定义类型
+		typedef struct point WCpoint;
+		
+		WCpoint p2 = {2,3}；
+		
+		//定义一个结构体数组
+		WCpoint[] arr = { {3,4},{3,6}};
+		
+		arr[1].i = 3;
+		
+		int arrLen = sizeof(arr)/sizeof(arr[0]);
+		//定义块变量
+		(void) ^(block)(WCpoint[]);
+		//讲块赋值块变量point
+		point ^(WCpoint[] arr) {
+			for (WCpoint* p = arr; p < arr + arrLen + ; p++) {
+				NSLog(@"is &@:" + *p.i);
+			}
+		}
+		
+		//调用块
+		point(arr);
+		
+	}
+	
+}
